897d937d335e01c161d315cee2f47797eb636143
snowyz12 <95395118+snowyz12@users.noreply.github.com>
2022-02-12 23:28:16 +0700

    Add files via upload

---
+++ src/components/formTodo.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container>
+    <v-col class="d-flex">
+      <v-select
+        :items="items"
+        v-model="subject"
+        label="Select"
+        solo
+        required
+      ></v-select>
+    </v-col>
+
+    <v-col>
+      <v-text-field
+        v-model="work"
+        label="Outlined"
+        outlined
+        clearable
+      ></v-text-field>
+    </v-col>
+    <v-btn class="mr-4" @click="addTodo" color="primary">Add</v-btn>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      subject: "",
+      work: "",
+      items: ["วิชา SE", "วิชา Web", "วิชา ML", "อื่นๆ"],
+    };
+  },
+  methods: {
+    addTodo() {
+      //console.log("click add todo");
+      console.log(this.subject + " " + this.work);
+      this.$emit("atd", this.addTodo);
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/views/Todo.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-container>
+    <h1>To do page</h1>
+    <h2>เพิ่มหัวข้องาน/การบ้าน</h2>
+
+    <FormTodo @atd="getTodo"></FormTodo>
+    <div class="container">
+      <v-container>
+        <h1>งาน/การบ้าน</h1>
+        <v-list flat subheader two-line>
+          <v-list-item-group v-model="settingsItem">
+            <v-list-item v-for="(item, i) in additems" :key="i">
+              <template fluid>
+                <v-list-item-action>
+                  <v-checkbox></v-checkbox>
+                </v-list-item-action>
+
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.text"></v-list-item-title>
+                </v-list-item-content>
+                <v-btn color="warning" @click="deleteTodo(i)">Delete</v-btn>
+              </template>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-container>
+    </div>
+  </v-container>
+</template>
+
+<script>
+import formTodo from "../components/formTodo.vue";
+import FormTodo from "../components/formTodo.vue";
+
+export default {
+  data: () => ({
+    subject: "",
+    work: "",
+    SettingsItem: 1,
+    additems: [
+      { text: "วิชา SE :ออกแบบ diagram" },
+      { text: "วิชา SE :ออกแบบ db" },
+      { text: "วิชา Web :ออกแบบ ui" },
+      { text: "วิชา Web :Assingment100" },
+    ],
+  }),
+  componentsL: { formTodo },
+  components: { FormTodo },
+  methods: {
+    getTodo() {
+      console.log("Todo");
+      this.additems.push({
+        text: this.subject + " " + this.work,
+      });
+    },
+    deleteTodo(index) {
+      this.additems.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<style></style>
